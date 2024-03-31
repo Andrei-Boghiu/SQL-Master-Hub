c6acc288c7660c2db156975347bca6ad57db0d2b
Andrew Schatten <vandrei1408@yahoo.com>
2024-03-31 12:07:56 +0300

    Updated file structure for auth and db migration

---
+++ src/app/(auth)/register/page.jsx
@@ -0,0 +1,23 @@
+// const RegisterPage = () => {
+// 	return (
+// 		<div>
+// 			<form action="">
+// 				<input
+// 					type="text"
+// 					placeholder="Username"
+// 				/>
+// 				<input
+// 					type="email"
+// 					placeholder="Email"
+// 				/>
+// 				<input
+// 					type="password"
+// 					placeholder="Password"
+// 				/>
+// 				<button type="submit">Register</button>
+// 			</form>
+// 		</div>
+// 	);
+// };
+
+// export default RegisterPage;
--- src/app/practice/page.jsx
+++ src/app/practice/page.jsx
@@ -8,7 +8,7 @@ import { useState, useEffect } from "react";
 import { Suspense } from "react";
 import Loading from "./loading";
 
-import { executeSelectQuery, executeSchemaInfoQuery } from "@/database/practice/connect";
+import { executeSelectQuery, executeSchemaInfoQuery } from "@/database/practice/railway";
 
 const Practice = () => {
 	const [query, setQuery] = useState("");
