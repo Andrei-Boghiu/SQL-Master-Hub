cf32d557908d02f77918009b5e3ee6e4867cb8d4
jandreiv <jandreiv@amazon.com>
2024-04-09 16:41:43 +0300

    ENV change

--- src/database/practice/vercelPostgres.js
+++ src/database/practice/vercelPostgres.js
@@ -5,7 +5,7 @@ import pg from 'pg';
 const { Pool } = pg;
 
 const pool = new Pool({
-    connectionString: process.env.POSTGRES_URL,
+    connectionString: process.env.VERCEL_POSTGRES_URL,
 });
 
 const organizeData = (data) => {
